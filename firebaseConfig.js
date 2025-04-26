6709c86b224a92a1f47b1d5312f56b843b196eb4
Shraiyash Pandey <shraiyash.pandey@gmail.com>
2025-04-25 22:20:59 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,14 +6,8 @@ import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore, collection } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWH9M2qeROmH6yTbB_FG-v08ASIpNbbTg",
-  authDomain: "sup-realtime-chat.firebaseapp.com",
-  projectId: "sup-realtime-chat",
-  storageBucket: "sup-realtime-chat.firebasestorage.app",
-  messagingSenderId: "473661278016",
-  appId: "1:473661278016:web:d47294a62f732a83ff5ca5"
 };
 
 // Initialize Firebase
